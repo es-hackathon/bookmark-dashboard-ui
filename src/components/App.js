4b34fe1f86f365aab7ed579b6d46a6f4b0f2129a
Amit P Naik <amit.naik8103@gmail.com>
2020-09-16 17:54:30 +0530

    feat: configure routing

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./home/HomePage";
+import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
+import ManageCardPage from './cards/ManageCardPage';
+import CardPage from "./cards/CardPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import GroupsPage from "./groups/GroupsPage";
+import ManageGroupPage from './groups/ManageGroupPage';
+
+function App() {
+  return (
+    <div className="container-fluid padding-align ">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/groups" component={GroupsPage} />
+        <Route exact path="/group/:id" component={ManageGroupPage} />
+        <Route exact path="/group" component={ManageGroupPage} />
+        <Route exact path="/cards" component={CardPage} />
+        <Route exact path="/card/:id" component={ManageCardPage} />
+        <Route exact path="/card" component={ManageCardPage} />
+        <Route exact component={PageNotFound} />
+      </Switch>
+      <ToastContainer autoClose={3000} hideProgressBar />
+    </div>
+  );
+}
+
+export default App;
