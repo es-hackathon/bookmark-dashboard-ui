2c7b8dee0a9ec57d8a26cd98d6daa67692e358b8
Amit P Naik <amit.naik8103@gmail.com>
2020-09-17 23:37:24 +0530

    feat: wrapped auth0 in app js and index js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
@@ -11,26 +11,45 @@ import "react-toastify/dist/ReactToastify.css";
 import GroupsPage from "./groups/GroupsPage";
 import ManageGroupPage from './groups/ManageGroupPage';
 import CardView from "./cardview/cardview";
+import AuthContext from "./AuthContext";
+import Auth from './auth/Auth';
+import Callback from './Callback';
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      auth: new Auth(this.props.history),
+
+    };
+  }
+  
+   render() {
+    const { auth } = this.state;
+    return (
+     <AuthContext.Provider value={auth}>
     <div className="container-fluid padding-align ">
-      <Header />
+      <Header auth={auth} />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/groups" component={GroupsPage} />
-        <Route exact path="/group/:id" component={ManageGroupPage} />
-        <Route exact path="/group" component={ManageGroupPage} />
-        <Route exact path="/cards" component={CardPage} />
-        <Route exact path="/card/:id" component={ManageCardPage} />
-        <Route exact path="/card" component={ManageCardPage} />
-        <Route exact path="/cardview/:id" component={CardView} />
-        <Route exact component={PageNotFound} />
+        <Route exact path="/" auth={auth} component={HomePage} />
+        <Route path="/callback"
+          render={props => <Callback auth={auth} {...props} />}/>
+        <Route exact auth={auth} path="/about" component={AboutPage} />
+        <Route exact auth={auth} path="/groups" component={GroupsPage} />
+        <Route exact auth={auth} path="/group/:id" component={ManageGroupPage} />
+        <Route exact auth={auth} path="/group" component={ManageGroupPage} />
+        <Route exact auth={auth} path="/cards" component={CardPage} />
+        <Route exact auth={auth} path="/card/:id" component={ManageCardPage} />
+        <Route exact auth={auth} path="/card" component={ManageCardPage} />
+        <Route exact auth={auth} path="/cardview/:id" component={CardView} />
+        <Route exact auth={auth} component={PageNotFound} />
       </Switch>
       <ToastContainer autoClose={3000} hideProgressBar />
     </div>
+   </AuthContext.Provider>
   );
+  }
 }
 
 export default App;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react'
 import { NavLink, Link } from "react-router-dom";
 
-const Header = () => {
+export default class Header extends Component {
+	render() {
     const activeStyle = { color: "#F15B2A" };
+    	const { isAuthenticated, login, logout, userHasScopes } = this.props.auth;
     return (
         <nav >
             <ul>
+	    	    {isAuthenticated() && (
                 <li>
                     <Link to="/" activestyle={activeStyle}>Home</Link>
                 </li>
+                )}
+                {isAuthenticated() && (
                 <li>
                     <Link to="/cards" activestyle={activeStyle}>Cards</Link>
                 </li>
+                )}
+                {isAuthenticated() && (
                 <li>
                     <Link to="/groups" activestyle={activeStyle}>Groups</Link>
                 </li>
+                )} 
                 <li>
                     <Link to="/about" activestyle={activeStyle}>About</Link>
+                </li>
+			    <li>
+                    <button onClick={isAuthenticated() ? logout : login}>
+                        {isAuthenticated() ? "Log Out" : "Log In"}
+                    </button>
                 </li>
             </ul>
         </nav>
     );
-};
-
-export default Header;
+	}
+}
